4a1fe81f7c39e1ab5db84810d3b0bb4f87bf31c7
roem9 <muhammadrum04@gmail.com>
2023-08-21 18:53:23 +0700

    sertifikat course

--- application/controllers/Sertifikat.php
+++ application/controllers/Sertifikat.php
@@ -42,6 +42,37 @@ class Sertifikat extends CI_Controller {
         // $this->load->view("pages/layout/footer");
     }
 
+    public function course_no($id){
+        $peserta = $this->Main_model->get_one("peserta_toefl", ["md5(id)" => $id]);
+        $peserta['link'] = $this->Main_model->get_one("config", ['field' => "web admin"]);
+        if($peserta){
+            $tes = $this->Main_model->get_one("tes", ["id_tes" => $peserta['id_tes']]);
+            $peserta['nama'] = $peserta['nama'];
+            $peserta['title'] = "Sertifikat Course ".$peserta['nama'];
+            $peserta['t4_lahir'] = ucwords(strtolower($peserta['t4_lahir']));
+            $peserta['tahun'] = date('Y', strtotime($tes['tgl_tes']));
+            $peserta['bulan'] = date('m', strtotime($tes['tgl_tes']));
+            $peserta['istima'] = poin("Listening", $peserta['nilai_listening']);
+            $peserta['tarakib'] = poin("Structure", $peserta['nilai_structure']);
+            $peserta['qiroah'] = poin("Reading", $peserta['nilai_reading']);
+            $peserta['tgl_tes'] = $tes['tgl_tes'];
+            $peserta['tgl_berakhir'] = date('Y-m-d', strtotime('+1 year', strtotime($tes['tgl_tes'])));
+
+            $peserta['link_foto'] = config();
+
+            $skor = ((poin("Listening", $peserta['nilai_listening']) + poin("Structure", $peserta['nilai_structure']) + poin("Reading", $peserta['nilai_reading'])) * 10) / 3;
+            $peserta['skor'] = $skor;
+
+            $peserta['no_doc'] = "TI-TW/{$peserta['bulan']}/{$peserta['tahun']}/{$peserta['no_doc_course']}";
+        }
+
+        // $this->load->view("pages/layout/header-sertifikat", $peserta);
+        // $this->load->view("pages/soal/".$page, $data);
+        $peserta['background'] = $this->Main_model->get_one("config", ["field" => 'background']);
+        $this->load->view("pages/sertifikat_course", $peserta);
+        // $this->load->view("pages/layout/footer");
+    }
+
     public function download($id){
         $peserta = $this->Main_model->get_one("peserta_toefl", ["md5(id)" => $id]);
         $tes = $this->Main_model->get_one("tes", ["id_tes" => $peserta['id_tes']]);
---
+++ application/views/pages/sertifikat_course.php
@@ -0,0 +1,60 @@
+<?php $this->load->view("_partials/header")?>
+    
+    <div class="wrapper" id="elementtoScrollToID">
+        <!-- <div class="sticky-top">
+            <?php $this->load->view("_partials/navbar-header")?>
+        </div> -->
+        <div class="page-wrapper" id="">
+            <div class="page-body">
+                <div class="container-xl">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-md-6">
+                                <?php if(isset($nama)) :?>
+                                    <div class="card shadow mb-4">
+                                        <div class="card-body text-gray-900">
+                                            <center>
+                                                <img src="<?= $link_foto[0]['value']?>/assets/img/logo.png?t=<?= time()?>" width=150 class="img-fluid mb-3" alt="">
+                                            </center>
+                                        <!-- </div>
+                                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between"> -->
+                                            <h5 class="m-0 font-weight-bold text-dark">No. Sertifikat : </i><?= $no_doc?></h5>
+                                        <!-- </div> -->
+                                        <!-- <div class="card-body text-gray-900"> -->
+                                            <p class="mt-3">
+                                                Nama : <?= $nama?><br>
+                                                TTL : <?= $t4_lahir?>, <?= tgl_indo($tgl_lahir)?><br>
+                                                Alamat : <?= ucwords(strtolower($alamat))?>
+                                            </p>
+                                            <p>
+                                                Telah menyelesaikan kursus TOEFL Intensive selama satu bulan di TOEFL Warrior dengan hasil sebagai berikut : 
+                                            </p>
+                                            <p>
+                                                Nilai Listening : <?= $istima?><br>
+                                                Nilai Structure : <?= $tarakib?><br>
+                                                Nilai Reading : <?= $qiroah?><br>
+                                                Skor TOEFL : <?= round($skor)?>
+                                            </p>
+                                            <p>
+                                                Tgl Menyelesaikan Kursus : <?= tgl_indo($tgl_tes)?><br>
+                                            </p>
+                                            <p><small class="text-danger"><i>Catatan : Data diatas adalah hasil yang diambil dari tugas akhir yang tingkatnya setara dengan real test TOEFL. Data tersebut merupakan data peserta kursus TOEFL Intensive yang sebenar-benarnya dengan tidak ada pengurangan maupun penambahan nilai sedikitpun.</i></small></p>
+                                        </div>
+                                    </div>
+                                <?php else :?>
+                                    <div class="card shadow mb-4">
+                                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                            <h6 class="m-0 font-weight-bold text-dark">Sertifikat Tidak Tersedia</i></h6>
+                                        </div>
+                                        <div class="card-body text-gray-900">
+                                            <div class="alert alert-warning"><i class="fa fa-exclamation-circle text-warning mr-3"></i>Maaf Sertifikat Anda Tidak Ditemukan</div>
+                                        </div>
+                                    </div>
+                                <?php endif;?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $this->load->view("_partials/footer")?>
